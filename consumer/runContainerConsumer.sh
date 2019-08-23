d36c6a026a352d3671026732089e3d58df7b80a9
Jerome Boyer <boyerje@us.ibm.com>
2019-08-23 06:25:51 -0700

    new consumer for containers events

--- consumer/runContainerConsumer.sh
+++ consumer/runContainerConsumer.sh
@@ -5,4 +5,4 @@ else
     cid=$1
 fi
 source ../scripts/setenv.sh NOSET
-docker run -e KAFKA_BROKERS=$KAFKA_BROKERS -e KAFKA_APIKEY=$KAFKA_APIKEY -e KAFKA_ENV=$KAFKA_ENV -v $(pwd)/..:/home --network=docker_default -ti ibmcase/python bash -c "cd /home/ContainersPython && export PYTHONPATH=/home && python ConsumeContainers.py $cid"
+docker run -e KAFKA_BROKERS=$KAFKA_BROKERS -e KAFKA_APIKEY=$KAFKA_APIKEY -e KAFKA_ENV=$KAFKA_ENV -v $(pwd)/..:/home -ti ibmcase/python bash -c "cd /home/consumer && export PYTHONPATH=/home && python ConsumeContainers.py $cid"
