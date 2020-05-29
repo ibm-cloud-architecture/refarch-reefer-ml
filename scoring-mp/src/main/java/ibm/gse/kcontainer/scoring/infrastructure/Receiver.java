a199280ba8e998618c2a963463d8339379f16062
Jesus Almaraz <almarazj@ie.ibm.com>
2020-05-29 20:02:34 +0200

    scoring prediction bypass (#63)

--- scoring-mp/src/main/java/ibm/gse/kcontainer/scoring/infrastructure/Receiver.java
+++ scoring-mp/src/main/java/ibm/gse/kcontainer/scoring/infrastructure/Receiver.java
@@ -21,8 +21,10 @@ public class Receiver {
     @Inject ScoringClient scoringClient;
 
     @Inject
-    @ConfigProperty(name = "mockup", defaultValue="no")
-    private String mockup;
+    @ConfigProperty(name = "predictions_enabled", defaultValue="false")
+    private String predictions_enabled;
+
+    boolean anomaly = false;
 
     @Incoming("reefer-telemetry")
     @Outgoing("containers")
@@ -39,23 +41,22 @@ public class Receiver {
         // Getting the Telemetry out of the event.
         Telemetry t = new Telemetry(te.getPayload());
 
-        // Calling the Anomaly Scoring service
-        ScoringResult scoringResult = scoringClient.callAnomalyScoring(t);
-        // Getting the Scoring Prediction Results
-        ScoringPredictionValues result = scoringResult.getScoringPredictionValues();
-        System.out.println("This is the prediction: " + result.getPrediction());
-        System.out.println("This is the probability: " + "[" + result.getMetrics()[0] + "," + result.getMetrics()[1] + "]");
-
-        boolean anomaly = false;
-
-        if ("yes".equalsIgnoreCase(mockup)){
-            // Mockup
-            int number = new Random().nextInt(10);
-            System.out.println("Number: " + number);
-            if (number > 6) anomaly = true;
-        }
-        else{
+        if ("true".equalsIgnoreCase(predictions_enabled)){
+            // Calling the Anomaly Scoring service
+            System.out.println("Calling the prediction services");
+            ScoringResult scoringResult = scoringClient.callAnomalyScoring(t);
+            // Getting the Scoring Prediction Results
+            ScoringPredictionValues result = scoringResult.getScoringPredictionValues();
+            System.out.println("This is the prediction: " + result.getPrediction());
+            System.out.println("This is the probability: " + "[" + result.getMetrics()[0] + "," + result.getMetrics()[1] + "]");
+            // Is there anomaly?
             anomaly = result.getPrediction().equalsIgnoreCase("Issue");
+        }
+        else {
+            // Mockup the prediction
+            System.out.println("Prediction services disabled. Mocking the call");
+            int number = new Random().nextInt(10);
+            if (number > 6) anomaly = true;
         }
 
         if (!anomaly){
